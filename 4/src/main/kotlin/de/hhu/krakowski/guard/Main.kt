af795e8bd6ec8d2bdb3cae99e97336f4b40d8c0d
Filip Krakowski <Filip.Krakowski@Uni-Duesseldorf.de>
2018-12-06 22:06:10 +0100

    Complete task 4

--- 4/src/main/kotlin/de/hhu/krakowski/guard/Main.kt
+++ 4/src/main/kotlin/de/hhu/krakowski/guard/Main.kt
@@ -1,10 +1,9 @@
 package de.hhu.krakowski.guard
 
-import java.awt.Event
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import kotlin.math.max
-import kotlin.math.min
+import java.util.stream.IntStream
+import kotlin.streams.toList
 
 val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
 
@@ -33,7 +32,7 @@ class State {
     }
 
     private fun process(event: FellAsleepEvent) {
-        fellAsleepAt()
+
     }
 
     private fun process(event: WokeUpEvent) {
@@ -48,12 +47,50 @@ fun main(args: Array<String>) {
             .lines()
             .filter { it.isNotBlank() }
 
+    // Task 1
     val events = input.map { parseEvent(it) }
             .filter { it.type != EventType.INVALID }
             .sortedBy { it.time }
-            .joinToString("\n")
 
-    println(events)
+    val guards = events.filter { it.type  == EventType.SHIFT_BEGAN }
+            .map { (it as ShiftBeganEvent).guardId }
+            .distinct()
+            .associate { Pair(it, mutableListOf<Duration>()) }
+
+    var currentGuard = 0
+    var startMinute = 0
+    for (event in events) {
+        when(event.type) {
+            EventType.SHIFT_BEGAN -> currentGuard = (event as ShiftBeganEvent).guardId
+            EventType.FELL_ASLEEP -> startMinute = event.time.minute
+            EventType.WOKE_UP -> guards[currentGuard]?.add(Duration(startMinute, event.time.minute))
+            EventType.INVALID -> {}
+        }
+    }
+
+    val firstGuard = guards.mapValues { it.value.fold(0) { acc, duration ->  acc + duration.getDifference()} }
+            .maxBy { it -> it.value } ?: return
+
+    val minute = guards[firstGuard.key]
+            ?.flatMap { IntStream.range(it.start, it.end).toList() }
+            ?.groupingBy { it }
+            ?.eachCount()
+            ?.maxBy { it.value }
+
+    println("Guard #${firstGuard.key} slept the most during minute ${minute?.key}.")
+
+    // Task 2
+    val secondGuard = guards
+            .mapValues { it.value.flatMap { IntStream.range(it.start, it.end).toList() } }
+            .mapValues { it.value.groupingBy { x -> x }.eachCount().maxBy { m -> m.value } }
+            .maxBy { it.value?.value ?: 0 } ?: return
+
+    println("Guard #${secondGuard.key} is most frequently asleep during minute ${secondGuard.value?.key}")
+
+}
+
+fun parseDateTime(time: String) : LocalDateTime {
+    return LocalDateTime.parse(time, dateFormatter)
 }
 
 fun parseEvent(event: String) : TimedEvent {
@@ -75,33 +112,17 @@ fun parseEvent(event: String) : TimedEvent {
     return InvalidEvent(LocalDateTime.MIN)
 }
 
-enum class EventType {
-    SHIFT_BEGAN, FELL_ASLEEP, WOKE_UP, INVALID
-}
-
-fun parseDateTime(time: String) : LocalDateTime {
-    return LocalDateTime.parse(time, dateFormatter)
-}
-
-open class TimedEvent (
-        val type: EventType,
-        val time: LocalDateTime
-)
-
-class ShiftBeganEvent(val guardId: Int, time: LocalDateTime) : TimedEvent(EventType.SHIFT_BEGAN, time) {
-    override fun toString(): String {
-        return "[${time}][SHIFT_CHANGE]{${guardId}}"
-    }
-}
-class FellAsleepEvent(time: LocalDateTime) : TimedEvent(EventType.FELL_ASLEEP, time) {
-    override fun toString(): String {
-        return "[${time}][FALLS_ASLEEP]"
-    }
-}
-class WokeUpEvent(time: LocalDateTime) : TimedEvent(EventType.WOKE_UP, time) {
-    override fun toString(): String {
-        return "[${time}][WAKES_UP]"
-    }
-}
+enum class EventType { SHIFT_BEGAN, FELL_ASLEEP, WOKE_UP, INVALID }
+open class TimedEvent (val type: EventType, val time: LocalDateTime)
+class ShiftBeganEvent(val guardId: Int, time: LocalDateTime) : TimedEvent(EventType.SHIFT_BEGAN, time)
+class FellAsleepEvent(time: LocalDateTime) : TimedEvent(EventType.FELL_ASLEEP, time)
+class WokeUpEvent(time: LocalDateTime) : TimedEvent(EventType.WOKE_UP, time)
 class InvalidEvent(time: LocalDateTime) : TimedEvent(EventType.INVALID, time)
+
+class Duration (val start: Int, val end: Int) {
+    fun getDifference() = end - start
+    override fun toString(): String {
+        return "(${start} - ${end})"
+    }
+}
 
